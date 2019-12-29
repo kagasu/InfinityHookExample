e618a65c17d3e3655d2bdcba42bead8a67795364
kagasu <admin@kagasu.net>
2019-12-29 15:50:30 +0900

    support NtOpenProcess

--- src/kinfinityhook/entry.cpp
+++ src/kinfinityhook/entry.cpp
@@ -20,8 +20,8 @@
 
 static wchar_t IfhMagicFileName[] = L"ifh--";
 
-static UNICODE_STRING StringNtCreateFile = RTL_CONSTANT_STRING(L"NtCreateFile");
-static NtCreateFile_t OriginalNtCreateFile = NULL;
+static UNICODE_STRING StringNtOpenProcess = RTL_CONSTANT_STRING(L"NtOpenProcess");
+static NtOpenProcess_t OriginalNtOpenProcess = NULL;
 
 /*
 *	The entry point of the driver. Initializes infinity hook and
@@ -46,12 +46,12 @@ extern "C" NTSTATUS DriverEntry(
 	DriverObject->DriverUnload = DriverUnload;
 
 	//
-	// Demo detouring of nt!NtCreateFile.
+	// Demo detouring of nt!NtOpenProcess
 	//
-	OriginalNtCreateFile = (NtCreateFile_t)MmGetSystemRoutineAddress(&StringNtCreateFile);
-	if (!OriginalNtCreateFile)
+	OriginalNtOpenProcess = (NtOpenProcess_t)MmGetSystemRoutineAddress(&StringNtOpenProcess);
+	if (!OriginalNtOpenProcess)
 	{
-		kprintf("[-] infinityhook: Failed to locate export: %wZ.\n", StringNtCreateFile);
+		kprintf("[-] infinityhook: Failed to locate export: %wZ.\n", StringNtOpenProcess);
 		return STATUS_ENTRYPOINT_NOT_FOUND;
 	}
 
@@ -103,15 +103,15 @@ void __fastcall SyscallStub(
 	UNREFERENCED_PARAMETER(SystemCallIndex);
 
 	//
-	// In our demo, we care only about nt!NtCreateFile calls.
+	// In our demo, we care only about nt!OpenProcess calls.
 	//
-	if (*SystemCallFunction == OriginalNtCreateFile)
+	if (*SystemCallFunction == OriginalNtOpenProcess)
 	{
 		//
 		// We can overwrite the return address on the stack to our detoured
-		// NtCreateFile.
+		// NtOpenProcess.
 		//
-		*SystemCallFunction = DetourNtCreateFile;
+		*SystemCallFunction = DetourNtOpenProcess;
 	}
 }
 
@@ -119,57 +119,22 @@ void __fastcall SyscallStub(
 *	This function is invoked instead of nt!NtCreateFile. It will 
 *	attempt to filter a file by the "magic" file name.
 */
-NTSTATUS DetourNtCreateFile(
-	_Out_ PHANDLE FileHandle,
-	_In_ ACCESS_MASK DesiredAccess,
-	_In_ POBJECT_ATTRIBUTES ObjectAttributes,
-	_Out_ PIO_STATUS_BLOCK IoStatusBlock,
-	_In_opt_ PLARGE_INTEGER AllocationSize,
-	_In_ ULONG FileAttributes,
-	_In_ ULONG ShareAccess,
-	_In_ ULONG CreateDisposition,
-	_In_ ULONG CreateOptions,
-	_In_reads_bytes_opt_(EaLength) PVOID EaBuffer,
-	_In_ ULONG EaLength)
+NTSTATUS DetourNtOpenProcess(
+	_Out_ PHANDLE             ProcessHandle,
+	_In_ ACCESS_MASK          DesiredAccess,
+	_In_ POBJECT_ATTRIBUTES   ObjectAttributes,
+	_In_ PCLIENT_ID           ClientId)
 {
 	//
 	// We're going to filter for our "magic" file name.
 	//
-	if (ObjectAttributes &&
-		ObjectAttributes->ObjectName && 
-		ObjectAttributes->ObjectName->Buffer)
+	if (ClientId->UniqueProcess == reinterpret_cast<HANDLE>(3900))
 	{
-		//
-		// Unicode strings aren't guaranteed to be NULL terminated so
-		// we allocate a copy that is.
-		//
-		PWCHAR ObjectName = (PWCHAR)ExAllocatePool(NonPagedPool, ObjectAttributes->ObjectName->Length + sizeof(wchar_t));
-		if (ObjectName)
-		{
-			memset(ObjectName, 0, ObjectAttributes->ObjectName->Length + sizeof(wchar_t));
-			memcpy(ObjectName, ObjectAttributes->ObjectName->Buffer, ObjectAttributes->ObjectName->Length);
-		
-			//
-			// Does it contain our special file name?
-			//
-			if (wcsstr(ObjectName, IfhMagicFileName))
-			{
-				kprintf("[+] infinityhook: Denying access to file: %wZ.\n", ObjectAttributes->ObjectName);
-
-				ExFreePool(ObjectName);
-
-				//
-				// The demo denies access to said file.
-				//
-				return STATUS_ACCESS_DENIED;
-			}
-
-			ExFreePool(ObjectName);
-		}
+		DesiredAccess = 0x8; // PROCESS_VM_OPERATION
 	}
 
 	//
 	// We're uninterested, call the original.
 	//
-	return OriginalNtCreateFile(FileHandle, DesiredAccess, ObjectAttributes, IoStatusBlock, AllocationSize, FileAttributes, ShareAccess, CreateDisposition, CreateOptions, EaBuffer, EaLength);
+	return OriginalNtOpenProcess(ProcessHandle, DesiredAccess, ObjectAttributes, ClientId);
 }
--- src/kinfinityhook/entry.h
+++ src/kinfinityhook/entry.h
@@ -20,18 +20,11 @@
 /// Structures and typedefs.
 ///
 
-typedef NTSTATUS(*NtCreateFile_t)(
-	_Out_ PHANDLE FileHandle,
-	_In_ ACCESS_MASK DesiredAccess,
-	_In_ POBJECT_ATTRIBUTES ObjectAttributes,
-	_Out_ PIO_STATUS_BLOCK IoStatusBlock,
-	_In_opt_ PLARGE_INTEGER AllocationSize,
-	_In_ ULONG FileAttributes,
-	_In_ ULONG ShareAccess,
-	_In_ ULONG CreateDisposition,
-	_In_ ULONG CreateOptions,
-	_In_reads_bytes_opt_(EaLength) PVOID EaBuffer,
-	_In_ ULONG EaLength);
+typedef NTSTATUS(*NtOpenProcess_t)(
+	_Out_ PHANDLE             ProcessHandle,
+	_In_ ACCESS_MASK          DesiredAccess,
+	_In_ POBJECT_ATTRIBUTES   ObjectAttributes,
+	_In_ PCLIENT_ID           ClientId);
 
 ///
 /// Forward declarations.
@@ -46,15 +39,8 @@ void __fastcall SyscallStub(
 	_In_ unsigned int SystemCallIndex, 
 	_Inout_ void** SystemCallFunction);
 
-NTSTATUS DetourNtCreateFile(
-	_Out_ PHANDLE FileHandle,
-	_In_ ACCESS_MASK DesiredAccess,
-	_In_ POBJECT_ATTRIBUTES ObjectAttributes,
-	_Out_ PIO_STATUS_BLOCK IoStatusBlock,
-	_In_opt_ PLARGE_INTEGER AllocationSize,
-	_In_ ULONG FileAttributes,
-	_In_ ULONG ShareAccess,
-	_In_ ULONG CreateDisposition,
-	_In_ ULONG CreateOptions,
-	_In_reads_bytes_opt_(EaLength) PVOID EaBuffer,
-	_In_ ULONG EaLength);
+NTSTATUS DetourNtOpenProcess(
+	_Out_ PHANDLE             ProcessHandle,
+	_In_ ACCESS_MASK          DesiredAccess,
+	_In_ POBJECT_ATTRIBUTES   ObjectAttributes,
+	_In_ PCLIENT_ID           ClientId);
